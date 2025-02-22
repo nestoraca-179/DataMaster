9a3f029170bdbfdbf6fa8f6d1185756f4036fa0d
alejandro <nestoraca.179@gmail.com>
2025-02-22 11:05:23 -0400

    ajuste al eliminar usuario

--- DataMaster/Models/MyUser.cs
+++ DataMaster/Models/MyUser.cs
@@ -42,14 +42,16 @@ namespace DataMaster.Models
 		{
 			Usuario old_user = GetUserByID(user.ID);
 
-			user.username = old_user.username;
-			user.password = old_user.password;
-			user.fec_camb = old_user.fec_camb;
-			user.co_us_mo = u;
-			user.fe_us_mo = DateTime.Now;
-			db.Entry(user).State = EntityState.Modified;
-			db.SaveChanges();
-			db.Entry(user).State = EntityState.Detached;
+			using (DataMasterEntities context = new DataMasterEntities())
+			{
+				user.username = old_user.username;
+				user.password = old_user.password;
+				user.fec_camb = old_user.fec_camb;
+				user.co_us_mo = u;
+				user.fe_us_mo = DateTime.Now;
+				context.Entry(user).State = EntityState.Modified;
+				context.SaveChanges();
+			}
 
 			user.password = GetChanges(old_user, user);
 			return user;
@@ -59,9 +61,12 @@ namespace DataMaster.Models
 		{
 			Usuario del_user = GetUserByID(id);
 
-			db.Usuario.Attach(del_user);
-			db.Usuario.Remove(del_user);
-			db.SaveChanges();
+			using (DataMasterEntities context = new DataMasterEntities())
+			{
+				context.Usuario.Attach(del_user);
+				context.Usuario.Remove(del_user);
+				context.SaveChanges();
+			}
 
 			return del_user;
 		}
