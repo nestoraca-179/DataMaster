4405867700b35b8c1de540fa06cfa426c96323bc
alejandro <nestoraca.179@gmail.com>
2025-03-04 20:33:59 -0400

    avances verificar pedidos

--- DataMaster/Controllers/OrderController.cs
+++ DataMaster/Controllers/OrderController.cs
@@ -64,5 +64,20 @@ namespace DataMaster.Controllers
 
 			return View();
         }
-    }
+    
+		public ActionResult VerificarPedido()
+		{
+			if (!Request.IsAuthenticated)
+				return RedirectToAction("Index", "Home", new { message = "Debes iniciar sesión" });
+			else if (Session["USER"] == null)
+				return RedirectToAction("Logout", "Account", new { msg = "Debes iniciar sesión" });
+
+			saSucursal s = (Session["BRANCH"] as saSucursal);
+			ViewBag.usuario = (Session["USER"] as Usuario);
+			ViewBag.empresa = Session["NAME_CONN"];
+			ViewBag.sucur = s?.sucur_des;
+
+			return View();
+		}
+	}
 }
